12ce33f217aaf55bb036032fa43d57e469c1ba98
Janardhan <janardhanbangalore007@gmail.com>
2023-12-14 22:39:20 +0530

    added_database_to_stores

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from flask import Flask
 from flask_smorest import Api
 from resources.stores import stores_blp
 from resources.items import items_blp
+from db import DataBase as db
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config["API_TITLE"] = "My API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.2"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["API_TITLE"] = "My API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.2"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-api = Api(app)
-api.register_blueprint(stores_blp)
-api.register_blueprint(items_blp)
+    with app.app_context():
+        db.initialize_db()
+    api = Api(app)
+    api.register_blueprint(stores_blp)
+    api.register_blueprint(items_blp)
+    return app
 
-
+app = create_app()
--- db.py
+++ db.py
@@ -1,2 +1,43 @@
-stores = {}
 items = {}
+import sqlite3
+
+class DataBase:
+    connection_string = "stores_db.db"
+
+    @classmethod
+    def initialize_db(cls,con_str = None):
+        if con_str is not None:
+            cls.connection_string = con_str
+        with sqlite3.connect(cls.connection_string) as con:
+            cur = con.cursor()
+            try:
+                cur.execute("""CREATE TABLE IF NOT EXISTS STORES (STORE_ID INTEGER, STORE_NAME VARCHAR NOT NULL, PRIMARY KEY (STORE_ID))""")
+                cur.execute("""CREATE TABLE IF NOT EXISTS ITEMS (ITEM_ID INTEGER, ITEM_NAME VARCHAR NOT NULL, ITEM_PRICE DECIMAL, STORE_ID INTEGER, PRIMARY KEY (ITEM_ID), FOREIGN KEY (STORE_ID) REFERENCES STORES(STORE_ID));""")
+            except sqlite3.OperationalError as e:
+                con.rollback()
+                print("Error while creating database schema table")
+                raise Exception("Error while creating database schema table")
+
+    @classmethod
+    def execute_query(cls,query):
+        with sqlite3.connect(cls.connection_string) as connection:
+            cursor = connection.cursor()
+            result = cursor.execute(query)
+            # try:
+            # except sqlite3.Error as e:
+            #     print(e)
+            #     raise Exception("Incorrect query")
+            # headers = [column[0] for column in cursor.description]
+            return result.fetchall()
+# print(DataBase.execute_query("SELECT * FROM STORES"))
+
+def jsonify(header, query_output, key=None):
+    if key is None:
+        result_list = [dict(zip(header,row)) for row in query_output]
+        return result_list
+    else:
+        result_dict = {}
+        for row in query_output:
+            row_json = dict(zip(header,row))
+            result_dict[row_json[key]] = row_json
+        return result_dict
